d0f30c2a037b9793c03b437ded5f52cbdc6125cd
Mayank Verma <44576058+Mayank909@users.noreply.github.com>
2023-06-30 15:23:10 +0530

    Revert "Feature Registration, Display and Edit "

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Registration from "./pages/Registration";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Nopage from "./pages/Nopage";
-import Home from "./pages/dashboard/Home";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Registration />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: ":userId", element: <Registration /> },
-    ],
-  },
-  { path: "*", element: <Nopage /> },
-]);
+import Registration from './pages/Registration';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div className="App">
+      <Registration/>
+    </div>
+  );
 }
 
 export default App;
